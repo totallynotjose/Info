f383fed2c698707ae7bb6b2322cc96e1b1905bc2
Jose <jfranke@stud.hs-bremen.de>
2023-06-20 19:16:54 +0200

    first version

---
+++ src/biomon/Main.java
@@ -0,0 +1,12 @@
+package biomon;
+
+public class Main {
+
+	//TODO add all loops
+	//TODO add methods from stats
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/biomon/Stats.java
@@ -0,0 +1,16 @@
+package biomon;
+
+public class Stats {
+	
+	//TODO add stats for biomon
+	//TODO add getter for a new biomon
+	//TODO add attack selection
+	//TODO add attacks for enemy
+	//TODO add own attacks
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
