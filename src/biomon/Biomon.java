a296cc7e63faf6ebf3a47f723419773888e4a54f
Jose <jfranke@stud.hs-bremen.de>
2023-06-22 15:40:58 +0200

    class stats deleted, everything is in class biomon now. Objects Biomon were added, type of own biomon can be chosen.

--- src/biomon/Biomon.java
+++ src/biomon/Biomon.java
@@ -5,16 +5,43 @@ package biomon;
 public class Biomon {
 
 	// all the stats the Biomon need
-	// other stats like damage (from attacks) and initiative will be drawn from
-	// "Stats"
 	// Biomon start at level 0 (equals level 1)
 	private int maxHP, currentHP, type;
 	private int level = 0;
+	
+	// stats for attacks
+	// normal attack, crit normal attack, special attack (levels 1 to 15; index 0 to 14)
+	// optional: would be nice to read these from a file to make it more readable
+	private int[][] fluffyStats = { { 10, 12, 14, 16, 18, 20, 21, 22, 23, 24, 26, 27, 28, 29, 30 },
+			{ 12, 14, 16, 18, 20, 25, 26, 27, 28, 29, 32, 33, 34, 35, 36 },
+			{ 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 33, 36, 39, 42, 44 } };
+	private int[][] slimyStats = { { 10, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 },
+			{ 13, 14, 15, 16, 17, 23, 24, 25, 26, 27, 31, 32, 33, 34, 35 },
+			{ 9, 10, 10, 11, 12, 14, 16, 16, 18, 20, 23, 26, 29, 32, 36 } };
+	private int[][] crispyStats = { { 10, 11, 12, 13, 14, 15, 16, 16, 17, 18, 19, 20, 21, 22, 23 },
+			{ 12, 13, 14, 16, 16, 17, 18, 18, 19, 20, 22, 23, 24, 25, 26 },
+			{ 8, 10, 12, 14, 16, 18, 19, 20, 22, 24, 26, 29, 32, 35, 38 } };
+
+	public static int chooseMyType() {
+		System.out.println("Choose your Biomon-type! (1) fluffy (2) slimy (3) crispy");
+		int decision = SystemInReader.readInt();
+
+		while (decision < 1 || decision > 3) {
+			System.out.println();
+			System.out.println("Invalid input, please try again: (1) fluffy (2) slimy (3) crispy");
+			decision = SystemInReader.readInt();
+		}
+		if (decision == 1) {
+			return 1;
+		} else if (decision == 2) {
+			return 2;
+		} else {
+			return 3;
+		}
+	}
 
 	// type 1 fluffy; type 2 slimy, type 3 crispy
-	// could be calculated through type as well -> type * 10 + level * 5 (no
-	// finetuning possible tho but faster and shorter)
-	public static int getMaxHP(int level, int type) {
+	public int calculateMaxHP() {
 		if (type == 1) {
 			return (10 + 5 * level);
 		} else if (type == 2) {
@@ -24,9 +51,33 @@ public class Biomon {
 		}
 	}
 
-	// TODO type gets chosen in here (not in main)
-	public Biomon() {
+	/*
+	 * print level and get level are two different methods because get level refers
+	 * to the index that we also use in arrays (stats for level one are index 0, so
+	 * the level starts at 0. But for printing you want the actual level, not the
+	 * index we will be using
+	 */
+	public int getLevel() {
+		return level;
+	}
 
+	public int printLevel() {
+		return (level + 1);
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public int getMaxHP() {
+		return maxHP;
+	}
+
+	public Biomon(int type, int level) {
+		this.type = type;
+		this.level = level;
+		maxHP = calculateMaxHP();
+		currentHP = maxHP;
 	}
 
 }
--- src/biomon/Main.java
+++ src/biomon/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	// TODO add all loops
 	// TODO add methods from stats
-	// TODO bring both methods (newFight and chooseOwnBiomon) to stats
-	// TODO make Biomon class with fields (not 500 ints with different infos
+	// TODO bring newFight to stats
+	// TODO make class where questions about fight will be dealt with
 
 	private static boolean newFight() {
 		System.out.println("Do you want to start the  next fight? (1) yes (2) no");
@@ -24,49 +24,28 @@ public class Main {
 
 	}
 
-	private static int chooseOwnBiomon() {
-		System.out.println("Choose your Biomon! (1) fluffy (2) slimy (3) crispy");
-		int decision = SystemInReader.readInt();
-
-		while (decision < 1 || decision > 3) {
-			System.out.println();
-			System.out.println("Invalid input, please try again: (1) fluffy (2) slimy (3) crispy");
-			decision = SystemInReader.readInt();
-		}
-		if (decision == 1) {
-			return 1;
-		} else if (decision == 2) {
-			return 2;
-		} else {
-			return 3;
-		}
-	}
-
 	public static void main(String[] args) {
 
 		boolean game = true; // game runs while true
 		boolean fight = true; // after every fight decide if you want another one (if not game ends)
 		// separation of game and fight because saving happens outside of the fight
-		int fightCounter = 0;
 
-		// choose type of Biomon (and print for checking), start with level 0
-		int myLevel = 0;
-		int myType = chooseOwnBiomon();
-		System.out.println("Type is " + myType + " and maxHP are " + Stats.getMaxHP(myLevel, myType));
+		// choose type of Biomon (and print for checking), start with level (index) 0
+		Biomon myBiomon = new Biomon(Biomon.chooseMyType(), 0);
+		System.out.println("My Biomon is type " + myBiomon.getType() + " and Level " + myBiomon.printLevel()
+				+ " and my maxHP are " + myBiomon.getMaxHP());
 		System.out.println();
 
 		while (game == true) {
-			fightCounter++;
 
 			// if fight == true make enemyBiomon here (HP, type and level)
 			if (fight == true) {
 				// type of enemy 1, 2 or 3
-				int enemyType = (int) (Math.random() * (3 - 1 + 1) + 1);
-				int enemyLevel = myLevel;
-				System.out.println("Enemy type is " + enemyType + " and level is " + (enemyLevel + 1)
-						+ " and max HP are " + Stats.getMaxHP(enemyLevel, enemyType));
+				Biomon enemyBiomon = new Biomon(2, myBiomon.getLevel());
 
-				System.out.println(fightCounter + ". fight!");
+				// as you level up with each fight your level is an indicator for the amounts of
+				// fights you had
+				System.out.println(myBiomon.printLevel() + ". fight!");
 			}
 
 			while (fight == true) {
