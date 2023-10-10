7a6e727f91c00e0d205332173b70ea1f7cae5522
Incontinenzia <kay.kiontke1@gmail.com>
2023-10-10 14:44:00 +0200

    Added new comments in the enums class

--- src/ressources/Enums.java
+++ src/ressources/Enums.java
@@ -1,5 +1,14 @@
 package ressources;
 
+/**
+ * This class allows a higher level of readibilty of the abstract code by giving each
+ * running state and save state a explicit name. Also the enums are applicable in the whole program.
+ * 
+ * @author Lennart Alexander Brockmann
+ * @author Josephine Franke
+ * @author Kay Kionkte
+ */
+
 public class Enums {
     public enum RunningStates {
         RUNNING, PAUSE, GAME_OVER
